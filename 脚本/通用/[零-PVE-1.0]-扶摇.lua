43940146f0e0e821d368399d606f436df7264905
TheZafkiel <61747242+TheZafkiel@users.noreply.github.com>
2024-03-04 17:47:02 +0800

    move

--- 脚本/通用/[零-PVE-1.0]-扶摇.lua
+++ 脚本/通用/[零-PVE-1.0]-扶摇.lua
@@ -1,23 +1,23 @@
---作者
+--????
 ZuoZhe = 'YUI'
---奇穴
+--???
 QiXue = ''
---其他说明
-ShuoMing = '自动补扶摇 自己跳 锈铁钩锁|景流.天地一|劈山尾鞭 读条50%会 ,输出会暂停3秒'
+--???????
+ShuoMing = '???????? ????? ????????|????.????|???β?? ????50%?? ,????????3??'
 
--- 扶摇状态 0 未起跳 1 起跳  3-顶点 二段 4准备小轻功
+-- ????? 0 δ???? 1 ????  3-???? ???? 4???С??
 FYZT = 0 --'123'
-FYZT_ARR_time = {} --起跳时间
---自动扶摇开关
+FYZT_ARR_time = {} --???????
+--??????????
 ZD_FYZT = true
--- 暂停技能释放到某时间
+-- ???????????????
 ZD_ZT_TIME = time()
 
 function Main()
    --  ZHIKONG_2()
 
     if  isRun() then
-        if totaprog() > 0.5 and tota('锈铁钩锁|景流.天地一|劈山尾鞭') then
+        if totaprog() > 0.5 and tota('????????|????.????|???β??') then
             suspension(3000)
             FYZT = 0
             jump_1()
@@ -27,21 +27,21 @@ function Main()
 end
 
 function DPS()
-    if   nobuff('弹跳') and nocd('扶摇直上') then
-        cast('扶摇直上')
+    if   nobuff('????') and nocd('??????') then
+        cast('??????')
     end
 end
 
--- 是否暂停
+-- ??????
 function isRun(arg1, arg2, arg3)
     return time() >= ZD_ZT_TIME
 end
--- 暂停 T 毫秒
+-- ??? T ????
 function suspension(t)
     ZD_ZT_TIME = t + time()
 end
 
--- 一段跳
+-- ?????
 function jump_1(params)
     if FYZT == 0 then
         -- statements
@@ -57,10 +57,10 @@ function jump_1(params)
     end
     if FYZT == 4 then
         FYZT_ARR_time[3] = time()
-        cast('蹑云逐月')
-        cast('凌霄揽胜')
-        cast('瑶台枕鹤')
-        cast('迎风回浪')
+        cast('????????')
+        cast('???????')
+        cast('???????')
+        cast('??????')
         FYZT = 5
     end
     if FYZT == 6 then
@@ -71,7 +71,7 @@ function jump_1(params)
 end
 
 --[[***
-  滞空 
+  ??? 
 **--]]
 function ZHIKONG_2(xqg)
     -- print('FYZT',FYZT, FYZT_ARR_time[2], time())
@@ -84,8 +84,8 @@ function ZHIKONG_2(xqg)
     --     )
     -- end
 
-    -- 保持滞空
-    if state('跳跃') and last('扶摇直上', 30) and nobuff('弹跳') and jumpc() == 1 and FYZT_ARR_time[1] == nil then
+    -- ???????
+    if state('???') and last('??????', 30) and nobuff('????') and jumpc() == 1 and FYZT_ARR_time[1] == nil then
         FYZT_ARR_time[1] = time()
         FYZT = 1.5
         if isRun() then
@@ -93,7 +93,7 @@ function ZHIKONG_2(xqg)
         end
     end
     if
-        state('跳跃') and last('扶摇直上', 30) and nobuff('弹跳') and jumpc() == 1 and --FYZT == 1 and
+        state('???') and last('??????', 30) and nobuff('????') and jumpc() == 1 and --FYZT == 1 and
             ((FYZT == 1 and time() - FYZT_ARR_time[1] >= 1600 and time() - FYZT_ARR_time[1] <= 1900) or
                 (FYZT == 1.5 and time() - FYZT_ARR_time[1] >= 1000 and time() - FYZT_ARR_time[1] <= 1900))
      then
@@ -102,7 +102,7 @@ function ZHIKONG_2(xqg)
     end
 
     if
-        state('跳跃') and last('扶摇直上', 30) and nobuff('弹跳') and jumpc() == 2 and
+        state('???') and last('??????', 30) and nobuff('????') and jumpc() == 2 and
             (FYZT == 3 and time() - FYZT_ARR_time[2] >= 600 and time() - FYZT_ARR_time[2] <= 1500)
      then
         FYZT = 4
@@ -114,7 +114,7 @@ function ZHIKONG_2(xqg)
         jump_1()
     end
 
-    if not (state('跳跃')) and jumpc() == 0 then
+    if not (state('???')) and jumpc() == 0 then
         FYZT = 0
         FYZT_ARR_time = {}
     end
